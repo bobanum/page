648cc61aa8c1f6bfc01087e3148ce77a5d5c874c
Martin Boudreau <mboudrea\cstj.qc.ca>
2024-09-02 09:56:13 -0400

    Temp

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
  */
 export default class Page extends HTMLElement {
     static tagName = "print-page";
-    static observedAttributes = ["rows", "cols", "orientation", "format", "margin", "marksStroke", "marksColor", "marksLength"];
+    static observedAttributes = ["rows", "cols", "orientation", "format", "margin", "marks"];
     constructor() {
         super();
         this.attachShadow({ mode: "open" });
@@ -13,36 +13,7 @@ export default class Page extends HTMLElement {
         this.shadowRoot.appendChild(this.dom.page());
         this.shadowRoot.appendChild(this.dom.marks());
     }
-    // static init() {
-    //     this.prototype.props = {
-    //         columns: 1,    // The number of columns on the page.
-    //         rows: 1,   // The number of rows on the page.
-    //         orientation: "portrait", // The orientation of the page (e.g., "portrait").
-    //         format: "letter", // The format of the page (e.g., "letter").
-    //         margin: "0.5in",    // The margin of the page (e.g., "0.5in").
-    //         // marksStroke: "0.5px", // The width of the page marks (e.g., "0.5in").
-    //         // marksColor: "black", // The color of the page marks (e.g., "black").
-    //         // marksLength: "1in", // The style of the page marks (e.g., "solid").    
-    //     };
 
-    //     const regularProps = ["orientation", "format", "margin", "marksStroke", "marksColor", "marksLength"];
-    //     for (let prop of regularProps) {
-    //         Object.defineProperty(this.prototype, prop, {
-    //             get: function () {
-    //                 return this.props[prop];
-    //             },
-    //             set: function (val) {
-    //                 this.props[prop] = val;
-    //             }
-    //         });
-    //     }
-
-    //     this.parseData(this.prototype, document.body);
-    //     this.formatBody();
-
-    //     this.addStylesheet();
-    //     this.addFontTheme();
-    // }
     apply() {
         if (this.domain === null) {
             return;
@@ -73,11 +44,6 @@ export default class Page extends HTMLElement {
             page.classList.add("page--page");
             page.appendChild(this.dom.slot(this.cols * this.rows));
             this.formatPage(page);
-
-            // page.appendChild(this.dom.marks(this.rows, this.columns));
-            // page.appendChild(this.parts(part, props.rows * props.columns));
-            // this.domain.parentNode.insertBefore(page, this.domain);
-            // this.domain.remove();
             return page;
         },
         slot: (n = 1) => {
@@ -98,7 +64,6 @@ export default class Page extends HTMLElement {
                     for (let i = 1; i < n; i += 1) {
                         slot.parentNode.insertBefore(duplicateContainer.cloneNode(true), slot.nextSibling);
                     }
-                    console.log('Slot content has changed.');
                 });
             }
             return slot;
@@ -125,12 +90,17 @@ export default class Page extends HTMLElement {
          * @returns {Element} - The container element with page marks.
          */
         marks: () => {
-            const { rows, cols } = this;
-            console.log(rows, cols);
+            const { rows, cols, marks } = this;
+            if (marks === "none" || rows * cols < 2) {
+                return document.createTextNode("");
+            }
 
             var result = document.createElement("div");
             result.classList.add("marks");
-            this.propsToCss('marks', result);
+            for (let prop in marks) {
+                result.style.setProperty(`--marks-${prop}`, marks[prop]);
+            }
+            // this.propsToCss('marks', result);
             var divRows = result.appendChild(document.createElement("div"));
             for (let c = 1; c < cols; c += 1) {
                 let group = divRows.appendChild(document.createElement("div"));
@@ -148,25 +118,6 @@ export default class Page extends HTMLElement {
             return result;
         }
     };
-    /**
-     * Parses data attributes of an element and stores them in an object.
-     * @param {Element} element - The element with data attributes to parse.
-     * @param {Object} obj - An object to store the parsed data attributes.
-     * @returns {Object} - The object with parsed data attributes.
-     */
-    static parseData(obj, ...elements) {
-        elements.forEach(element => {
-            for (const prop in element.dataset) {
-                if (Object.hasOwnProperty.call(element.dataset, prop)) {
-                    obj[prop] = element.dataset[prop];
-                }
-            }
-        });
-        return obj;
-    }
-    parseData(...elements) {
-        return this.constructor.parseData(this, ...elements);
-    }
     /**
      * Gets the number of columns on the page.
      * @returns {number} - The number of columns.
@@ -204,21 +155,40 @@ export default class Page extends HTMLElement {
         }
         return undefined;
     }
-    /**
-     * Formats a page element with the specified properties.
-     * @param {Element} page - The page element to format.
-     * @param {Object} props - The properties for formatting the page.
-     */
-    static formatBody() {
-        var size = this.getSize();
-        if (this.orientation) {
-            document.body.style.setProperty("page", this.orientation);
+    get marks() {
+        const result = {
+            width: 1,
+            length: 24,
+            style: "solid",
+            color: "hsla(0, 0%, 0%, 10%)"
+        };
+        if (!this.hasAttribute("marks")) {
+            return result;
         }
-        document.body.style.setProperty("--width", size.width + "pt");
-        document.body.style.setProperty("--height", size.height + "pt");
-        document.body.style.setProperty("--margin", this.margin);
-        document.body.style.setProperty("--cols", this.columns);
-        document.body.style.setProperty("--rows", this.rows);
+        const marks = this.getAttribute("marks").toLowerCase().trim();
+        if (marks === "none") {
+            return "none";
+        }
+        
+        const parts = marks.split(/\s+/);
+        const sizes = parts.filter(p => p.match(/^[\d+-.]+[a-z]*$/));
+        
+        if (sizes.length > 0) {
+            result.length = this.toPoints(sizes[1]) || result.length;
+        }
+        if (sizes.length > 1) {
+            result.width = this.toPoints(sizes[0]) || result.width;
+        }
+        const style = parts.find(p => p.match(/solid|dashed|dotted/));
+        if (style) {
+            result.style = style;
+        }
+        const color = parts.find(p => p.match(/^#[0-9a-f]{3,8}|[a-z]+\(.+\)$/));
+        if (color) {
+            result.color = color;
+        }
+        
+        return result;
     }
     formatPage(page) {
         var size = this.getSize();
@@ -266,35 +236,34 @@ export default class Page extends HTMLElement {
      * @param {Object} [obj=this] - The object with format and orientation information.
      * @returns {{width: number, height: number}} - The size of the page in points.
      */
-
     getSize() {
         var width = 0, height = 0;
         switch (this.format) {
             case "letter":
-                width = this.toPts(8.5, "in");
-                height = this.toPts(11, "in");
+                width = this.toPoints(8.5, "in");
+                height = this.toPoints(11, "in");
                 break;
             case "legal":
-                width = this.toPts(8.5, "in");
-                height = this.toPts(14, "in");
+                width = this.toPoints(8.5, "in");
+                height = this.toPoints(14, "in");
                 break;
             case "ledger":
             case "tabloid":
-                width = this.toPts(11, "in");
-                height = this.toPts(17, "in");
+                width = this.toPoints(11, "in");
+                height = this.toPoints(17, "in");
                 break;
             case "a3":
-                width = this.toPts(297, "mm");
-                height = this.toPts(420, "mm");
+                width = this.toPoints(297, "mm");
+                height = this.toPoints(420, "mm");
                 break;
             case "a4":
-                width = this.toPts(210, "mm");
-                height = this.toPts(297, "mm");
+                width = this.toPoints(210, "mm");
+                height = this.toPoints(297, "mm");
                 break;
             default:
                 let [w, wu, h, hu] = /([0-9.]+)([a-z]+)X([0-9.]+)([a-z]+)/i.exec(this.format).slice(1);
-                width = this.toPts(w, wu);
-                height = this.toPts(h, hu);
+                width = this.toPoints(w, wu);
+                height = this.toPoints(h, hu);
         }
         if (this.hasAttribute("orientation")) {
             if (this.orientation === "landscape" && width < height || this.orientation === "portrait" && width > height) {
@@ -309,37 +278,23 @@ export default class Page extends HTMLElement {
      * @param {string} unit - The unit of the value (e.g., "in", "mm").
      * @returns {number} - The converted value in points (pt).
      */
-    toPts(val, unit) {
+    toPoints(val, unit) {
+        if (typeof val === "string") {
+            unit = val.match(/[a-z]*$/i)[0];
+            val = parseFloat(val);
+        }
+        
+        unit = unit?.toLowerCase() || "pt";
+        
         const PTS = { pt: 1, in: 72, pc: 12, px: .75, mm: 2.83465, cm: 28.3465, dm: 283.465, m: 2834.65 };
+        // TODO Manage %, ch and ex units
+        // get body computed font size
+        PTS.em = parseFloat(getComputedStyle(document.body).fontSize) * PTS.px;
         val = parseFloat(val);
-        unit = unit.toLowerCase();
         if (PTS[unit] === undefined) {
             throw `Unrecognized length unit '${unit}'`;
         }
         return val * PTS[unit];
-    }
-    propsToCss(prefix, element) {
-        for (let prop in this.props) {
-            if (prop.startsWith(prefix)) {
-                let name = prop.slice(prefix.length).toLowerCase();
-                let value = this.props[prop];
-                element.style.setProperty(`--${name}`, value);
-            }
-        }
-    }
-    static from(...domains) {
-        const result = [];
-        for (let domain of domains) {
-            if (typeof domain === "string") {
-                domain = [...document.querySelectorAll(domain)];
-            }
-            if (Array.isArray(domain)) {
-                result.push(...domain.map(d => this.from(d)).flat());
-                continue;
-            }
-            result.push(new Page(domain));
-        }
-        return result;
     }
     static init(meta) {
         this.meta = meta;
